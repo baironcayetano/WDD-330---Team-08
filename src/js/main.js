0db96efe3b745156eaa965cd6015add70418ed95
baironcayetano <baironjuega01@gmail.com>
2026-09-20 00:31:39 -0600

    used the command npm run lint and npm run format.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 import ProductData from "./ProductData.mjs";
-import ProductList from "./productList.mjs";
+import ProductList from "./ProductList.mjs";
 
 const category = "tents";
 const listElement = document.querySelector(".product-list");
 
 const dataSource = new ProductData(category);
-const list = new ProductList(category, dataSource,listElement);
+const list = new ProductList(category, dataSource, listElement);
 list.init();
-
